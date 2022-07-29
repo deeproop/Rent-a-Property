c1f83a3a26541859a3980c826f15216bdb58bd22
Deep Roop <deepsinghroop@gmail.com>
2022-07-30 00:15:50 +0530

    .

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,11 +3,11 @@ import "./navbar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import {} from "@fortawesome/free-regular-svg-icons";
+<script src="https:kit.fontawesome.com/a076d05399.js"></script>
 const navbar = () => {
   return (
     <header>
-      
-        <FontAwesomeIcon icon={faHouse} color="#7065ee"/> Estatery
+      <div className="logo"><FontAwesomeIcon icon={faHouse} color="#7065ee"/><strong> Estatery</strong></div>
       <nav>
         <ul className="nav__links">
         <li><a href="#">Rent</a></li>
@@ -17,10 +17,16 @@ const navbar = () => {
         <li><a href="#">Resources</a></li>
         </ul>
       </nav>
-      <a className="cta" href="#"><button>Login</button></a>
+
+      <div className="buttons">
+      <a className="cta" href="#"><button className="login">Login</button></a>
       <a className="cta" href="#"><button>Signup</button></a>
+      </div>
+      
     </header>
   );
 };
 
 export default navbar;
+
+<label for="" className="button">Manage Property</label>
